d1a855f9c648af50c73a7df14b453cc0c57b5abb
Ubuntu <ubuntu@ip-172-31-39-160.ap-south-1.compute.internal>
2022-09-10 06:45:58 +0000

    :sparkles: Advanced Query Frontend Created

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // In this version of express, for sending requests using form-data, we need to add multer on our server-side
 const multer  = require('multer');
+// postgres
+const {Client} = require('pg');
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "postgres",
+    database: "reports"
+})
+
+client.connect();
+
+client.query(`Select * from public.dim_queuemsgs order by messagedate desc limit 100;`, (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message);
+    }
+    client.end;
+})
 
 const app = express();
+// multer config
 const upload = multer();
 app.use(upload.array());
 // parse application/x-www-form-urlencoded
